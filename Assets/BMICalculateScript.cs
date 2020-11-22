2b9faec56ce50f92277975f7cfc2f1c31e0027ab
MrKomugiko <Mr.Komugiko@gmail.com>
2020-11-22 00:18:05 +0000

    Added simple BMI calculator and chart represent colored values deppend of weight and height

---
+++ Assets/BMICalculateScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BMICalculateScript : MonoBehaviour
+{
+    [SerializeField] TMP_InputField HeightInput;
+    [SerializeField] TMP_InputField WeightInput;
+    [SerializeField] TextMeshProUGUI BMIResult;
+    public void OnClick_CalculateBMI(){
+
+        float height = float.Parse(HeightInput.text)/100;
+        float weight = float.Parse(WeightInput.text);
+
+        float BMI = weight/(height*height);
+        print($"wzrost: [{height}]");
+        print($"waga: [{weight}]");
+
+        print($"Twoje BMI to: [{Math.Round(BMI,2)}]");
+
+        BMIResult.SetText(Math.Round(BMI,2).ToString());
+    }
+
+    public float GetBMI(float weight, float height) => (float)Math.Round(weight/(height/100*height/100),1);
+}
---
+++ Assets/GridGeneratorScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GridGeneratorScript : MonoBehaviour
+{
+    public BMICalculateScript BMICalculator;
+    [SerializeField] GameObject GridCell;
+    [SerializeField] int chartSize = 15;
+
+    [SerializeField] float startHeight = 150;
+    [SerializeField] float endHeight = 200;
+    float heightIncrementValue => (endHeight-startHeight)/chartSize;
+    [SerializeField] float startWeight = 50;
+    [SerializeField] float endWeight = 120;
+    float weightIncrementValue => (endWeight-startWeight)/chartSize;
+
+    void Start()
+    {
+        BMICalculator = GameObject.FindObjectOfType<BMICalculateScript>().GetComponent<BMICalculateScript>();
+        for (int i = chartSize; i > 0; i--)
+        {
+            for (int j = 0; j < chartSize; j++)
+            {
+                float BMI = BMICalculator.GetBMI((startWeight+(weightIncrementValue*i)),(startHeight+(heightIncrementValue*j)));
+                var singleGridCell = Instantiate(GridCell);
+                singleGridCell.transform.SetParent(this.transform);
+               // singleGridCell.GetComponentInChildren<TextMeshProUGUI>().SetText($"[i{i},j{j}]");
+                if(BMI%1 == 0f)
+                {
+                    singleGridCell.GetComponentInChildren<TextMeshProUGUI>().SetText(BMI.ToString()+",0");
+                    print(BMI);
+                }
+                else
+                {
+                    singleGridCell.GetComponentInChildren<TextMeshProUGUI>().SetText(BMI.ToString());
+                }
+
+                singleGridCell.transform.localScale = Vector3.one;
+            }
+        }
+    }
+}
